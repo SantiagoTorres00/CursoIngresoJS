89b3a5d7cec8cdfd0bc54b66dde8f89176a94744
santiagotorres00 <santiagotorres6718@gmail.com>
2021-07-05 17:10:14 -0300

    hecho

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+  
+  var primerNumero = parseInt (document.getElementById('txtIdNumeroUno').value);
+
+  var segundoNumero =  parseInt (document.getElementById('txtIdNumeroDos').value);
+  
+  var resulto = primerNumero + segundoNumero;
+
+alert ("El resultado es: " + resulto);
+
+
+
+/*  
+var n1 = parseInt (document.getElementById('txtIdNumeroUno').value);
+var n2 = parseInt (document.getElementById('txtIdNumeroDos').value);
+alert 
+  
 }
 
